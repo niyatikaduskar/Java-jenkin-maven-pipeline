d5d08323441d70b597f9ded180b9f31651a519f7
niyati.waikar <niyati.waikar@gmail.com>
2021-10-05 16:39:28 +0530

    first-commit

--- src/main/java/com/api/controllers/APIController.java
+++ src/main/java/com/api/controllers/APIController.java
@@ -23,7 +23,7 @@ public class APIController {
 	
    @GetMapping("/greet")
    public String greet() {
-      return "Spring boot is working";
+      return "Spring boot is workingfine";
    }
    
    @GetMapping("/static-people")
